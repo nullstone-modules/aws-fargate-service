40196792e836e50407e3ceb93c01cbfae0165102
Brad Sickles <brad.sickles@gmail.com>
2020-08-28 18:11:45 -0400

    Adding support for tf 0.13

--- logs.tf
+++ logs.tf
@@ -10,7 +10,7 @@ resource "aws_cloudwatch_log_group" "this" {
 
 module "lb_logs_bucket" {
   source  = "cloudposse/lb-s3-bucket/aws"
-  version = "0.4.0"
+  version = "0.7.0"
 
   name   = "${var.stack_name}-${var.env}-${var.block_name}"
   region = data.aws_region.this.name
