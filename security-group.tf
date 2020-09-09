9c3af21c528ed72c86a42c4e6e7d7a0891d8fcb0
Brad Sickles <brad.sickles@gmail.com>
2020-09-09 09:39:49 -0400

    Ensuring intra-service communication

--- security-group.tf
+++ security-group.tf
@@ -17,3 +17,12 @@ resource "aws_security_group_rule" "this-https-to-world" {
   to_port           = 443
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "this-http-from-private-subnets" {
+  security_group_id = aws_security_group.this.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = data.terraform_remote_state.network.outputs.private_cidrs
+}
