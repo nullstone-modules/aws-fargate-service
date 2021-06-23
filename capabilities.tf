8a0668631af56e452f7e271d56b8d680f41e893a
Brad Sickles <brad.sickles@gmail.com>
2021-06-23 16:26:25 -0400

    rules instead of group

--- capabilities.tf
+++ capabilities.tf
@@ -5,12 +5,16 @@ locals {
 }
 
 locals {
-  datastores      = jsondecode(file("capabilities/datastores.json"))
+  datastores = jsondecode(file("capabilities/datastores.json"))
+
   ds_secrets_deep = [for ds in local.datastores : ds.secrets if ds.secrets != null]
   ds_secrets      = flatten(local.ds_secrets_deep)
-  ds_env_deep     = [for ds in local.datastores : ds.env if ds.env != null]
-  ds_env          = flatten(local.ds_env_deep)
-  ds_sec_groups   = [for ds in local.datastores : ds.security_group if ds.security_group != null]
+
+  ds_env_deep = [for ds in local.datastores : ds.env if ds.env != null]
+  ds_env      = flatten(local.ds_env_deep)
+
+  ds_sec_group_rules_deep = [for ds in local.datastores : ds.security_group_rules if ds.security_group_rules != null]
+  ds_sec_group_rules      = flatten(local.ds_sec_group_rules_deep)
 }
 
 locals {
@@ -19,23 +23,23 @@ locals {
 }
 
 resource "aws_security_group_rule" "this-to-datastore" {
-  count = length(local.ds_sec_groups)
+  count = length(local.ds_sec_group_rules)
 
-  security_group_id        = local.ds_sec_groups[count.index].id
+  security_group_id        = local.ds_sec_group_rules[count.index].id
   type                     = "egress"
-  from_port                = local.ds_sec_groups[count.index].port
-  to_port                  = local.ds_sec_groups[count.index].port
-  protocol                 = local.ds_sec_groups[count.index].protocol
+  from_port                = local.ds_sec_group_rules[count.index].port
+  to_port                  = local.ds_sec_group_rules[count.index].port
+  protocol                 = local.ds_sec_group_rules[count.index].protocol
   source_security_group_id = aws_security_group.this.id
 }
 
 resource "aws_security_group_rule" "datastore-from-this" {
-  count = length(local.ds_sec_groups)
+  count = length(local.ds_sec_group_rules)
 
   security_group_id        = aws_security_group.this.id
   type                     = "ingress"
-  from_port                = local.ds_sec_groups[count.index].value.port
-  to_port                  = local.ds_sec_groups[count.index].port
-  protocol                 = local.ds_sec_groups[count.index].protocol
-  source_security_group_id = local.ds_sec_groups[count.index].id
+  from_port                = local.ds_sec_group_rules[count.index].port
+  to_port                  = local.ds_sec_group_rules[count.index].port
+  protocol                 = local.ds_sec_group_rules[count.index].protocol
+  source_security_group_id = local.ds_sec_group_rules[count.index].id
 }
