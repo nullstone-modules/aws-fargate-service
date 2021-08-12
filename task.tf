11eb77f7322380b1d4b11a84366a4046a9cd92ec
Brad Sickles <brad.sickles@gmail.com>
2021-08-12 15:17:29 -0400

    Ensuring execution role is fully configured before allowing to create task

--- task.tf
+++ task.tf
@@ -46,6 +46,7 @@ resource "aws_ecs_task_definition" "this" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.execution.arn
+  depends_on               = [aws_iam_role_policy.execution]
   container_definitions    = jsonencode([local.container_definition])
   tags                     = data.ns_workspace.this.tags
 }
