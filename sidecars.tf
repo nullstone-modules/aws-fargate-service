56c8440a83a07f9a0d3703a0e09862f986bf5450
Brad Sickles <brad.sickles@gmail.com>
2021-09-29 17:31:48 -0400

    fmt

--- sidecars.tf
+++ sidecars.tf
@@ -23,5 +23,5 @@ locals {
 
   // If a sidecar takes over the service_port, we will configure the load balancer against that container instead of "main"
   sidecars_owns_service_port = { for s in local.sidecars : s.name => tobool(lookup(s, "owns_service_port", false)) }
-  lb_container_name          = try(compact([ for name, owns in local.sidecars_owns_service_port : (owns == true ? name : "") ])[0], "main")
+  lb_container_name          = try(compact([for name, owns in local.sidecars_owns_service_port : (owns == true ? name : "")])[0], "main")
 }
