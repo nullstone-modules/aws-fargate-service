9b5a7bfcb628ab8d51e56d66edc5d3bcbd101ecb
Brad Sickles <brad.sickles@gmail.com>
2021-09-29 17:32:18 -0400

    Correcting capability support for log configurations, add secretOptions

--- logs.tf
+++ logs.tf
@@ -5,3 +5,23 @@ module "logs" {
   tags              = data.ns_workspace.this.tags
   enable_log_reader = true
 }
+
+locals {
+  cw_log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      "awslogs-region"        = data.aws_region.this.name
+      "awslogs-group"         = module.logs.name
+      "awslogs-stream-prefix" = local.block_name
+    }
+  }
+
+  cap_log_configurations = lookup(local.capabilities, "log_configurations", [])
+  addl_log_configurations = [for lc in local.cap_log_configurations : {
+    logDriver     = lc.logDriver
+    options       = jsondecode(lookup(lc, "options", "{}"))
+    secretOptions = jsondecode(lookup(lc, "secretOptions", "{}"))
+  }]
+
+  log_configurations = concat(local.addl_log_configurations, [local.cw_log_configuration])
+}
