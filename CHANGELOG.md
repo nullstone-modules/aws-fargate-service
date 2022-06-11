d110d52784a3f61b6599fc32519e1364c52b5a8a
Brad Sickles <brad.sickles@gmail.com>
2022-06-11 09:22:17 -0400

    contract

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+# 0.12.5 (Jun 11, 2022)
+* Upgrade `ns_connection` to use `contract` instead of `type`.
+
+# 0.12.4 (Jun 07, 2022)
+* Fixed `random_string` to use `numeric` instead of deprecated `number`.
+* Added description to `region` output.
+
+# 0.12.3 (May 23, 2022)
+* Created unique secret names that don't collide with deletes.
+
 # 0.12.2 (May 13, 2022)
 * Added `NULLSTONE_PUBLIC_HOSTS` and `NULLSTONE_PRIVATE_HOSTS` to app env vars.
 * Added `public_hosts` and `private_hosts` outputs.
--- nullstone.tf
+++ nullstone.tf
@@ -22,28 +22,3 @@ locals {
   block_name    = data.ns_workspace.this.block_name
   resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
 }
-
-data "ns_connection" "cluster" {
-  name = "cluster"
-  type = "cluster/aws-fargate"
-}
-
-data "ns_connection" "network" {
-  name = "network"
-  type = "network/aws"
-  via  = data.ns_connection.cluster.name
-}
-
-locals {
-  vpc_id               = data.ns_connection.network.outputs.vpc_id
-  private_cidrs        = data.ns_connection.network.outputs.private_cidrs
-  public_cidrs         = data.ns_connection.network.outputs.public_cidrs
-  cluster_name         = data.ns_connection.cluster.outputs.cluster_name
-  deployers_name       = data.ns_connection.cluster.outputs.deployers_name
-  service_domain       = data.ns_connection.network.outputs.service_discovery_name
-  service_discovery_id = data.ns_connection.network.outputs.service_discovery_id
-}
-
-data "aws_ecs_cluster" "cluster" {
-  cluster_name = local.cluster_name
-}
