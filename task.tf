e767506070269a3b37f4b013313328d6286fe13d
Brad Sickles <brad.sickles@gmail.com>
2022-09-29 15:17:22 -0400

    Add support for cap namespace for env vars/secrets (#34)

--- task.tf
+++ task.tf
@@ -1,18 +1,10 @@
 locals {
-  standard_env_vars = tomap({
-    NULLSTONE_ENV           = data.ns_workspace.this.env_name
-    NULLSTONE_PUBLIC_HOSTS  = join(",", local.public_hosts)
-    NULLSTONE_PRIVATE_HOSTS = join(",", local.private_hosts)
-  })
-
   main_container_name = "main"
 
   // has_port_mapping dictates whether the main container should map a port to the network
   // If the user specifies service_port=0, then that will disable
   // If a capability sidecar specifies as owns_service_port=true, then that will also disable
   disable_main_port_mapping = !(var.service_port > 0) || anytrue(values(local.sidecars_owns_service_port))
-
-  env_vars = [for k, v in merge(local.standard_env_vars, var.service_env_vars) : { name = k, value = v }]
 
   container_definition = {
     name      = local.main_container_name
@@ -26,7 +18,7 @@ locals {
       }
     ]
 
-    environment = concat(local.env_vars, try(local.capabilities.env, []))
+    environment = local.env_vars
     secrets     = local.secret_refs
 
     mountPoints = local.mount_points
@@ -45,7 +37,7 @@ resource "aws_ecs_task_definition" "this" {
   execution_role_arn       = aws_iam_role.execution.arn
   depends_on               = [aws_iam_role_policy.execution]
   container_definitions    = jsonencode(concat([local.container_definition], local.addl_container_defs))
-  tags                     = data.ns_workspace.this.tags
+  tags                     = local.tags
   task_role_arn            = aws_iam_role.task.arn
 
   dynamic "volume" {
