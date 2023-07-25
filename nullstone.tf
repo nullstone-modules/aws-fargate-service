5b1b5e2f3d0baa733e330f58d08f1e584c018a64
Brad Sickles <brad.sickles@gmail.com>
2023-07-25 12:09:15 -0400

    drop unused

--- nullstone.tf
+++ nullstone.tf
@@ -8,11 +8,6 @@ terraform {
 
 data "ns_workspace" "this" {}
 
-data "ns_env" "this" {
-  stack_id = data.ns_workspace.this.stack_id
-  env_id   = data.ns_workspace.this.env_id
-}
-
 // Generate a random suffix to ensure uniqueness of resources
 resource "random_string" "resource_suffix" {
   length  = 5
@@ -23,8 +18,7 @@ resource "random_string" "resource_suffix" {
 }
 
 locals {
-  tags           = data.ns_workspace.this.tags
-  block_name     = data.ns_workspace.this.block_name
-  resource_name  = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
-  is_preview_env = data.ns_env.this.type == "PreviewEnv"
+  tags          = data.ns_workspace.this.tags
+  block_name    = data.ns_workspace.this.block_name
+  resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
 }
