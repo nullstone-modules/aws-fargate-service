93b8c2fc23e460beaaa4b7f9f1649b57ade96eaf
Brad Sickles <brad.sickles@gmail.com>
2024-01-15 12:30:10 -0500

    Added metrics outputs

--- outputs.tf
+++ outputs.tf
@@ -24,6 +24,16 @@ output "log_reader" {
   sensitive   = true
 }
 
+output "metrics_reader" {
+  value       = module.logs.reader
+  description = "object({ name: string, access_key: string, secret_key: string }) ||| An AWS User with explicit privilege to read metrics from Cloudwatch."
+  sensitive   = true
+}
+
+output "metrics_mappings" {
+  value = local.metrics_mappings
+}
+
 output "image_repo_name" {
   value       = try(aws_ecr_repository.this[0].name, "")
   description = "string ||| "
