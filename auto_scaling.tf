4022512ae693e68118e9ff1183542b073dc65492
Brad Sickles <brad.sickles@gmail.com>
2025-11-05 10:36:19 -0500

    use num_tasks for min capacity, new max_tasks for max capacity

--- auto_scaling.tf
+++ auto_scaling.tf
@@ -1,11 +1,6 @@
-locals {
-  min_capacity = try([for x in local.capabilities.auto_scaling : x.min_capacity if x.enabled][0], var.num_tasks)
-  max_capacity = try([for x in local.capabilities.auto_scaling : x.max_capacity if x.enabled][0], var.num_tasks)
-}
-
 resource "aws_appautoscaling_target" "this" {
-  min_capacity       = local.min_capacity
-  max_capacity       = local.max_capacity
+  min_capacity       = var.num_tasks
+  max_capacity       = var.max_tasks
   resource_id        = "service/${local.cluster_name}/${local.block_name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
